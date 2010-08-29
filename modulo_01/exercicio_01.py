9eec2d18cf17422572543d316d5c5f931c14991c
Joao P Dubas <joao_dubas@yahoo.com.br>
2010-08-29 09:57:12 -0300

    Inseridas pequenas melhorias no programa de adivinhacao

--- modulo_01/exercicio_01.py
+++ modulo_01/exercicio_01.py
@@ -5,17 +5,24 @@
 Importa o módulo random e sorteia um número inteiro entre 1 e 100
 '''
 import random
-numero = random.randint(1, 100)
 
-escolha, tentativas = 0, 0
+numero = random.randint(1, 100) # numero sorteado aleatoriamente
+
+escolha, tentativas = 0, 0  # definindo o numero de tentativas e a escolha do usuario
+
+mensagem = "O numero %d e %s do que o numero sorteado"  # mensagem apresentada ao usuario
+
+posicao_escolha = ""    # variavel a ser usada para definir se escolha > || < numero
 
 while escolha != numero:
     escolha = input("Escolha um numero entre 1 e 100: ")
     tentativas += 1
 
     if escolha < numero:
-        print "O numero %d e menor do que o numero sorteado." % escolha
+        posicao_escolha = "menor"
     elif escolha > numero:
-        print "O numero %d e maior do que o numero sorteado." % escolha
+        posicao_escolha = "maior"
+    
+    print mensagem % (escolha, posicao_escolha)
 
 print "Parabens! Voce acertou com %d tentativas." % tentativas
